0536087b6d6d5efe905b65b93238a7fe674d07d3
VishakhaSoti <vishakhasoti@gmail.com>
2026-06-18 10:39:42 +0530

    Modules Committed

---
+++ MODULES/maths.py
@@ -0,0 +1,29 @@
+import math
+num1 = 25
+sqrt = math.sqrt(num1)
+print(sqrt)
+
+num2 = 4
+power = math.pow(num2, 3)
+print(power)
+
+num3 = 10
+fctrl = math.factorial(num3)
+print(fctrl)
+
+#-----------------------------------#
+import math
+x = -10.7
+y = 3
+print(math.ceil(x))       
+print(math.floor(x))
+print(math.fabs(x))     
+print(pow(y,2))
+
+#-----------------------------#
+
+import math
+def sqrt(x):
+    y = int(math.sqrt(x))
+    print("the sqrt of given num is", y)
+sqrt(4)
